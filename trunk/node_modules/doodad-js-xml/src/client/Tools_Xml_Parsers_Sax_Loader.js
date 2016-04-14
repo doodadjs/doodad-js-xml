96002342859466cef0b7c6daa1de93e228484df5
doodad-js <doodad-js@f4e9449a-2e7a-49bc-89ce-d1f85e87716c>
2016-04-14 06:42:34 +0000

    - <Fix> all packages: Fix install - <Fix> doodad-js: Multiple fixes on modules load. - <Fix> doodad-js, doodad-js-io: When running under node.js (server-side), now forces "tools.log" output to "stderr" instead of "stdout". - <Fix> doodad-js: "tools.getCurrentScript": Missing dependency. - <Fix> doodad-js: "types.toSource": Minor fix. - <Fix> doodad-js-locale: "unicode.isClass": Now the test applies to the whole string. - <Fix> doodad-js-terminal: Fix insert when navigating the commands history. - <Chg> all packages: Multiple changes on the packages build process. - <Chg> all packages: Remove the extra "settings" section from module options. - <Chg> doodad-js: "namespaces.loadNamespaces" renamed to "namespaces.load". An alias as been created to keep compatility. - <Chg> doodad-js: "files.Path.prototype.toString": Now clears the "dirChar" option when the "os" option is specified. - <Chg> doodad-js: "tools.getOS": Now auto-detect "case sensitive" when server-side. Added a module option to change it. - <Chg> doodad-js: "tools.getDefaultLanguage": Now auto-detect when server-side. - <Chg> doodad-js-make: Rewritten. - <Chg> doodad-js-minifiers: Rewritten. - <Chg> doodad-js-terminal: Commands can now be extended. - <New> doodad-js: "types.depthExtend" now accepts an extender function. - <New> doodad-js: "files.Path.prototype.relative": Convets an absolute path to a relative one.

--- trunk/node_modules/doodad-js-xml/src/client/Tools_Xml_Parsers_Sax_Loader.js
+++ trunk/node_modules/doodad-js-xml/src/client/Tools_Xml_Parsers_Sax_Loader.js
@@ -1,4 +1,4 @@
-//! REPLACE_BY("// Copyright 2016 Claude Petit, licensed under Apache License version 2.0\n")
+//! REPLACE_BY("// Copyright 2016 Claude Petit, licensed under Apache License version 2.0\n", true)
 // dOOdad - Object-oriented programming framework
 // File: Tools_Xml_Parsers_Sax_Loader.js - Loader for SAX parser (client-side)
 // Project home: https://sourceforge.net/projects/doodad-js/
@@ -27,16 +27,21 @@
 	var global = this;
 	
 	var exports = {};
-	if (typeof process === 'object') {
-		module.exports = exports;
+	
+	//! BEGIN_REMOVE()
+	if ((typeof process === 'object') && (typeof module === 'object')) {
+	//! END_REMOVE()
+		//! IF_DEF("serverSide")
+			module.exports = exports;
+		//! END_IF()
+	//! BEGIN_REMOVE()
 	};
+	//! END_REMOVE()
 	
 	exports.add = function add(DD_MODULES) {
 		DD_MODULES = (DD_MODULES || {});
 		DD_MODULES['Doodad.Tools.Xml.Parsers.Sax.Loader'] = {
-			type: null,
-			//! INSERT("version:'" + VERSION('doodad-js-xml') + "',")
-			namespaces: null,
+			version: /*! REPLACE_BY(TO_SOURCE(VERSION(MANIFEST("name")))) */ null /*! END_REPLACE() */,
 			dependencies: [
 				'Doodad.Tools.Xml',
 			],
@@ -123,8 +128,23 @@
 		return DD_MODULES;
 	};
 	
-	if (typeof process !== 'object') {
-		// <PRB> export/import are not yet supported in browsers
-		global.DD_MODULES = exports.add(global.DD_MODULES);
+	//! BEGIN_REMOVE()
+	if ((typeof process !== 'object') || (typeof module !== 'object')) {
+	//! END_REMOVE()
+		//! IF_UNDEF("serverSide")
+			// <PRB> export/import are not yet supported in browsers
+			global.DD_MODULES = exports.add(global.DD_MODULES);
+		//! END_IF()
+	//! BEGIN_REMOVE()
 	};
-}).call((typeof global !== 'undefined') ? global : ((typeof window !== 'undefined') ? window : this));
+	//! END_REMOVE()
+}).call(
+	//! BEGIN_REMOVE()
+	(typeof window !== 'undefined') ? window : ((typeof global !== 'undefined') ? global : this)
+	//! END_REMOVE()
+	//! IF_DEF("serverSide")
+	//! 	INJECT("global")
+	//! ELSE()
+	//! 	INJECT("window")
+	//! END_IF()
+);
